bac5bb3475c71e8284e54df2ed17e47ee8d33f9b
TYOPXN360 <TYOPXN360@Gmail.com>
2026-06-12 13:44:17 +0800

    chore: bump version to 1.5.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ty.wifikeyxposed"
         minSdk = 30
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.5.6"
+        versionCode = 10
+        versionName = "1.5.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
